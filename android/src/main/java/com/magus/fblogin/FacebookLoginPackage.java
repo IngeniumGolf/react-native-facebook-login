936f4926f7368f4226bc09eae89e6a7b50fe72c2
plougsgaard <thomas@plougsgaard.dk>
2018-04-13 11:51:34 +0200

    Needed for our purposes.

--- android/src/main/java/com/magus/fblogin/FacebookLoginPackage.java
+++ android/src/main/java/com/magus/fblogin/FacebookLoginPackage.java
@@ -16,12 +16,7 @@ public class FacebookLoginPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(new FacebookLoginModule(reactContext));
     }
-
-    // Deprecated from RN 0.47.0
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
+    
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
